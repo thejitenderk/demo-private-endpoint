206e8b2583834cd3fb4e6e45abc37042662f9293
Jitender Kumar <thejitenderk@gmail.com>
2026-07-04 17:43:43 +0530

    service endpoint code

---
+++ demo-service-endpoint/modules/storage_account/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  network_rules {
+    default_action             = "Deny"
+    virtual_network_subnet_ids = var.subnet_ids
+  }
+
+}
---
+++ demo-service-endpoint/modules/virtual_network/output.tf
@@ -0,0 +1,3 @@
+output "vnetnames" {
+  value = azurerm_virtual_network.virtual_network.name
+}
